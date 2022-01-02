5182be01288c9d1bf81c998affbaa0eddc2d340f
choyondas <choyondas08@gmail.com>
2022-01-02 22:48:17 +0800

    front page

---
+++ src/Components/widgetLg/WidgetLg.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./WidgetLg.css";
+const WidgetLg = () => {
+  return <div className="WidgetLg"
+  >
+      large</div>;
+};
+
+export default WidgetLg;
---
+++ src/fakedata.js
@@ -0,0 +1,75 @@
+
+ export  const userData = [
+    {
+      name: 'Jan',
+      "Active User": 1200,
+     
+      amt: 2400,
+    },
+    {
+      name: 'Feb',
+      "Active User": 2300,
+    
+      amt: 2210,
+    },
+    {
+      name: 'Mar',
+      "Active User": 2400,
+      
+      amt: 2290,
+    },
+    {
+      name: 'Apr',
+      "Active User": 3280,
+     
+      amt: 2000,
+    },
+    {
+      name: 'May',
+      "Active User": 1890,
+    
+      amt: 2181,
+    },
+    {
+      name: 'Jun',
+      "Active User": 2890,
+      
+      amt: 2500,
+    },
+    {
+      name: 'Jul',
+      "Active User": 2190,
+     
+      amt: 2160,
+    },
+    {
+      name: 'Aug',
+      "Active User": 3120,
+      
+      amt: 2500,
+    },
+    {
+      name: 'Sep',
+      "Active User": 2390,
+    
+      amt: 2100,
+    },
+    {
+      name: 'Oct',
+      "Active User": 2690,
+      
+      amt: 3400,
+    },
+    {
+      name: 'Nov',
+      "Active User": 1990,
+      
+      amt: 2300,
+    },
+    {
+      name: 'Dec',
+      "Active User": 3090,
+      
+      amt: 2100,
+    },
+  ];
