df1fba23b9993b3e1f242187fc2fc8b58ed3578b
choyondas <choyondas08@gmail.com>
2022-01-02 16:04:17 +0800

    sdlk

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-        
-      <h2>Hello I am choyon my part is to design dashboard</h2>
+      <h2>lets make dashboard</h2>
     </div>
   );
 }
