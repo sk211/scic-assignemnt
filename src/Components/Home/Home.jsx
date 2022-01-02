0c3753beca994b5fc4c29d2e38f1ca08652afca5
sk211 <sujanmajumder211@gmail.com>
2022-01-02 13:44:50 +0600

    add new content

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            <h2>Helcome to SCIC Assignment team  work</h2>
+            <h2>welcome to SCIC Assignment team  work</h2>
 
         </div>
     )
