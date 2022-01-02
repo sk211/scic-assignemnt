be8fe95f9dc52776e5bbf213a55335036df96ea3
choyondas <choyondas08@gmail.com>
2022-01-02 19:14:19 +0800

    left side

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Dashboard, LineStyle, Timeline, TrendingUp } from "@material-ui/icons";
+import { AttachMoney, BarChart, ChatBubbleOutline, Dashboard, DynamicFeed, LineStyle, MailOutline, PermIdentity, Report, Storefront, Timeline, TrendingUp, WorkOutline } from "@material-ui/icons";
 import React from "react";
 import "./Sidebar.css";
 
@@ -9,17 +9,72 @@ const Sidebar = () => {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <Dashboard />
+            <li className="sidebarListItem active">
+              <Dashboard className="sidebarIcon" />
               home
             </li>
             <li className="sidebarListItem">
-              <Timeline />
+              <Timeline className="sidebarIcon" />
               analytics
             </li>
             <li className="sidebarListItem">
-              <TrendingUp />
+              <TrendingUp className="sidebarIcon" />
               Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <PermIdentity className="sidebarIcon" />
+              Users
+            </li>
+            <li className="sidebarListItem ">
+              <Storefront className="sidebarIcon" />
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoney  className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <BarChart  className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <MailOutline  className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeed className="sidebarIcon" />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutline className="sidebarIcon" />
+              Message
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <WorkOutline  className="sidebarIcon" />
+             Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <Report  className="sidebarIcon" />
+              Reports
             </li>
           </ul>
         </div>
