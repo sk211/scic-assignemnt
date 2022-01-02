0cda05b2f13ac05b3052c2323133231c7e6ea9ee
choyondas <choyondas08@gmail.com>
2022-01-02 15:44:30 +0800

    test

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-    return (
-        <div>
-            <h2>Helcome to SCIC Assignment team  work</h2>
-
-        </div>
-    )
+  return (
+    <div>
+        
+      <h2>Hello I am choyon my part is to design dashboard</h2>
+    </div>
+  );
 }
